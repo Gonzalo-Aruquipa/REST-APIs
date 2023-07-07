1d7b762f88666f3d571714bc460a8b685b790fb7
Gonzalo Aruquipa <gonzao777info@gmail.com>
2023-07-07 19:52:00 -0400

    end user register and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,44 @@
-const User = require("../models/User")
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
-exports.register = async(req, res) =>{
-  
+exports.register = async (req, res) => {
   const user = new User(req.body);
   try {
+    const salt = await bcrypt.genSaltSync(10);
+    user.password = await bcrypt.hashSync(user.password, salt);
     await user.save();
     res.status(200).send("user created");
   } catch (error) {
-    console.log(error)
+    res.status(400).send(error.message);
   }
+};
 
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
 
-}
+  const user = await User.findOne({ email: email });
 
-exports.login = () => {
-  
-}
+  if (user) {
+    const validPassword = await bcrypt.compareSync(password, user.password);
+    if (validPassword) {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          name: user.name,
+          perfil: user.perfil,
+          email: user.email,
+        },
+        "top_secret",
+        {
+          expiresIn: "1d",
+        }
+      );
+      res.send(token);
+    } else {
+      res.send("Incorrect password");
+    }
+  } else {
+    res.status(401).send("Invalid User");
+  }
+};
--- models/User.js
+++ models/User.js
@@ -16,6 +16,10 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true
+  },
+  perfil: {
+    type: String,
+    required: true
   }
 
 })
